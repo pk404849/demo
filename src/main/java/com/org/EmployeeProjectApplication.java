62ef76f66f13a93682ffebfb53b29538a2a4fd12
Kumar <prince.n.kumar@capgemini.com>
2024-07-03 17:37:25 +0530

    second commit

--- src/main/java/com/org/EmployeeProjectApplication.java
+++ src/main/java/com/org/EmployeeProjectApplication.java
@@ -9,6 +9,7 @@ public class EmployeeProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeProjectApplication.class, args);
 		System.out.println("==================== main method completed successfully ======================");
+		System.out.println("main method not completed");
 	}
 
 }
